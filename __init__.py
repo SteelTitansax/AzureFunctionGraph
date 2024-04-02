2c6cde98dea0bf29f18c92303169f695b0c3d83d
SteelTitansax <112019917+SteelTitansax@users.noreply.github.com>
2024-04-02 23:11:57 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -19,13 +19,11 @@ def main(mytimer: func.TimerRequest) -> None:
     token_url = 'https://login.microsoftonline.com/<DIRECTORY (tenant) ID>/oauth2/token'
     
     token_data = {
-     ‘grant_type’: ‘password’,
+     ‘grant_type’: ‘client_credentials’,
      ‘client_id’: app_id,
      ‘client_secret’: client_secret,
      ‘resource’: ‘https://graph.microsoft.com',
-     ‘scope’:’https://graph.microsoft.com',
-     ‘username’:’john.doe@companyxxx.com’, #Account with no 2MFA
-     ‘password’:’Happy2020’,
+     ‘scope’:’https://graph.microsoft.com'
     }
 
     token_r = requests.post(token_url, data=token_data)
